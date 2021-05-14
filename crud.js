bd773e8147a6925a6c991b3af5423d924d088d2e
x3meuriel <x3me7@hotmail.com>
2021-05-14 11:10:18 -0500

    adding client pg

--- crud.js
+++ crud.js
@@ -12,19 +12,7 @@ const db = knex({
       ssl: true
     }
   });
-
-
-  const { Client } = require('pg');
-
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-    
   
-
 
   
 
